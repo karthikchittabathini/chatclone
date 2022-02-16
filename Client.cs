7186bc688ce96d2a377dcfbb23242be0af57c67c
karthikchittabathini <chittabathinikarthik@gmail.com>
2022-02-16 10:15:54 +0530

    files slightly modified

--- Client.cs
+++ Client.cs
@@ -7,23 +7,27 @@ using System.Text.RegularExpressions;
 
 namespace HandsOnProject
 {
+    
     public class Client
     {
-
+        #region gettersetters/accessors
         public String ClientName { get; set; }
         public long ClientPhNo { get; set; }
+        #endregion
 
-        public Client(String name , long No)
+        #region client constructor
+        public Client(String name, long No)
         {
             ClientName = name;
             ClientPhNo = No;
         }
-        
+        #endregion
 
     }
 
     public class ClientRegister
     {
+        #region methods
         public bool PhoneNumberValidation(string num)
         {
             string text = num;
@@ -36,49 +40,78 @@ namespace HandsOnProject
             else
             {
                 //Console.WriteLine("valid Phone number");
-                return true;    
+                return true;
             }
         }
-
+        #endregion
 
     }
+
+
+    public class message
+    {
+        
+        server sr = new server();
+        #region methods
+        public void Messaging()
+        {
+            
+            Console.WriteLine("Please enter the UserName of the person you want to send message");
+            string name = Console.ReadLine();
+            foreach (var x in sr.clients)
+            {
+                if (x.ClientName == name)
+                {
+                    Console.WriteLine("Enter the message you want to send");
+                    string msg = Console.ReadLine();
+                    Message m = new Message(msg, ProgramServer.active_user , name);
+                    sr.MessagesList.Add(m);
+
+                }
+                else
+                {
+                    Console.WriteLine("There is no user with the name you entered");
+                }
+            }
+        }
+        #endregion methods
+
+    }
+
 
     public class ContactBookInClient
     {
         public List<Client> ContactBook = new List<Client>();
         server sr = new server();
+        #region methods
         public void AddToContactBook(string n, long phno)
-        {
-            string num = phno.ToString();
-            ClientRegister cr = new ClientRegister();
-            if (cr.PhoneNumberValidation(num))
             {
+                string num = phno.ToString();
+                ClientRegister cr = new ClientRegister();
+                if (cr.PhoneNumberValidation(num))
+                {
 
-                Client cb1 = new Client(n, phno);
-                if (!sr.CheckExistingUserOrNot(cb1, ContactBook))
-                    sr.clients.Add(cb1);
+                    Client cb1 = new Client(n, phno);
+                    if (!sr.CheckExistingUserOrNot(cb1))
+                        sr.clients.Add(cb1);
                     ContactBook.Add(cb1);
+                }
             }
-        }
+        
 
         public void DisplayContactBook()
-        {
-            foreach (var x in ContactBook)
             {
-                Console.WriteLine(x.ClientName + " " + x.ClientPhNo);
+                foreach (var x in ContactBook)
+                {
+                    Console.WriteLine(x.ClientName + " " + x.ClientPhNo);
+                }
             }
-        }
+
+        #endregion
     }
 
-    public class Messages
-    {
-        List<string> Message = new List<string>();
-        public  string MessageTosend(string msg)
-        {
-            Message.Add(msg);
-            return msg;
-        }
-
-    }
 
 }
+
+
+
--- server.cs
+++ server.cs
@@ -6,26 +6,69 @@ using System.Threading.Tasks;
 
 namespace HandsOnProject
 {
+    public class Message
+    {
+        #region GettersSetters
+
+        public String msg { get; set; }
+        public string sender { get; set; }
+        public string receiver { get; set; }
+
+        public DateTime dt { get; set; }
+        #endregion
+
+        #region constructors
+        public Message(String m, string s, string r)
+        {
+            msg = m;
+            sender = s;
+            receiver = r;
+            dt = DateTime.Now;
+        }
+        #endregion
+
+    }
     public class server
     {
         //list that contains the customers details(All Data)
 
         public List<Client> clients = new List<Client>();
-        
 
+        public List<Message> MessagesList = new List<Message>();
 
+        #region methods
         //checking UserExistingorNot in current clients list
-        public bool CheckExistingUserOrNot(Client client, List<Client> lst)
+        public bool CheckExistingUserOrNot(Client client)
         {
-            foreach (var x in lst)
+            foreach (var x in clients)
             {
-                if (x.ClientPhNo == client.ClientPhNo && x.ClientName == client.ClientName)
+                if (x.ClientPhNo == client.ClientPhNo || x.ClientName == client.ClientName)
                 {
                     return true;
                 }
             }
             return false;
         }
+
+
+        //To delete a existing user
+        public void DeleteExistingUser()
+        {
+            Console.WriteLine("please enter Registered user name : ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter the registered Phone Number");
+            long phno = Convert.ToInt64(Console.ReadLine());
+            Client cl = new Client(name, phno);
+            foreach (var x in clients.ToArray())
+            {
+                if (x.ClientPhNo == cl.ClientPhNo && x.ClientName == cl.ClientName)
+                {
+                    clients.Remove(x);
+                }
+            }
+        }
+
+
 
         public void DisplayUsersDetails()
         {
@@ -35,12 +78,7 @@ namespace HandsOnProject
                 Console.WriteLine(x.ClientName + " " + x.ClientPhNo);
             }
         }
-
-        public void MessageServiceFromSenderToReceiver(string msg, string clientname)
-        {
-
-        }
-
+        #endregion
 
     }
 }
